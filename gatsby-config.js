150fdd0de7e303a133b3e615c003211a615b018b
Mathieu Delisle <37332321+jmdtc@users.noreply.github.com>
2019-10-29 12:41:23 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
         resolve: 'gatsby-source-prismic-graphql',
         options: {
-            repositoryName: 'portfolioo', // (REQUIRED, replace with your own)
+            repositoryName: 'portfolio-mathieu-delisle', // (REQUIRED, replace with your own)
             linkResolver: () => post => `/${post.uid}`,
         }
     },
